5cf90d1691759107caa4b27b3f77819a812681ea
Mustafa Koca <acokmustafa123@gmail.com>
2024-09-02 08:17:06 +0300

    Update sweep_category_lists.py
    Potansiyel iyileştirmeler

--- sweep_category_lists.py
+++ sweep_category_lists.py
@@ -2,25 +2,28 @@
 
 import os
 import apt
-os.chdir("usr/share/linuxmint/mintinstall/categories")
 
+# Doğru yolu kontrol edin
+os.chdir("/usr/share/linuxmint/mintinstall/categories")
 
+# Paket önbelleğini yükleyin
 c = apt.Cache()
 
+# Kategorideki her dosya için işlemleri başlatın
 for file in os.listdir():
     new = []
-    with open(os.path.join(os.getcwd(), file)) as f:
+    file_path = os.path.join(os.getcwd(), file)
+    with open(file_path, "r") as f:
         for line in f:
-            if line.startswith("flatpak:"):
+            if line.startswith("flatpak:") or line == "\n":
                 new.append(line)
-                continue
-            if line == "\n":
-                new.append(line)
-                continue
-            try:
-                pkg = c[line.rstrip()]
-                new.append(line)
-            except KeyError as e:
-                print(f"missing {line}", end="")
-    with open(os.path.join(os.getcwd(), file), "w") as f:
+            else:
+                pkg_name = line.rstrip()
+                if pkg_name in c:
+                    new.append(line)
+                else:
+                    print(f"missing {pkg_name}")
+    
+    # Değişiklikleri yazmak için dosyayı açın
+    with open(file_path, "w") as f:
         f.write("".join(new))
